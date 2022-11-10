4e4222d52dedcd4e3d42e3c1a77b7d1aa54d0c92
koc234245 <114868607+koc234245@users.noreply.github.com>
2022-11-10 22:31:43 +0600

    Update recursion3.py
    bug fix

--- recursion3.py
+++ recursion3.py
@@ -5,9 +5,6 @@ def revers(x, c):
         return c
     elif x > 0:
         d = x % 10
-        print(d)
-        print(x)
-        print(c)
         return revers(x // 10, c * 10 + d)
 
 print(revers(user, 0))
